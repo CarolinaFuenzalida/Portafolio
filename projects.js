252f8e5c3d8662e5255b393cdf9d802f3523dcb5
Carolina Fuenzalida <108352521+CarolinaFuenzalida@users.noreply.github.com>
2023-01-24 11:47:09 -0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,15 +1,22 @@
 export default {
-    "projects": [
+    "projects": [ 
         {
-            "name": "Cipher - Spy X Family",
-            "about": "Aplicación web que permite al usuario cifrar y descifrar mensajes aplicando la técnica de Cifrado Cesar. Utilizando HTML, CSS Y JS Vainilla.",
-            "time": "Junio 2022",
-            "image": "https://user-images.githubusercontent.com/108352521/212223642-8b49ca18-71e2-4601-92f7-2bc421ac4904.png",
-            "demo": "https://carolinafuenzalida.github.io/SCL021-cipher/src/index.html",
-            "repo": "https://github.com/CarolinaFuenzalida/SCL021-cipher"
+                "name": "Sanrio Coffee",
+                "about": "Web app con vistas de mesero para tomar pedidos y vista de cocina para visualizar los pedidos a preparar, manejo de data a través de Firebase. Utilizando React.",
+                "time": "Noviembre 2022",
+                "image": "https://user-images.githubusercontent.com/108352521/208190958-940046b3-fc7e-4736-b872-9eaa2076d864.png",
+                "demo": "https://sanriocoffee.netlify.app/",
+                "repo": "https://github.com/CarolinaFuenzalida/SCL021-burger-queen",
             },
-
-            {
+         {
+                "name": "GG-Girls Social Network",
+                "about": "Red social GG-Girls enfocada en comunidad Gamer de Mujeres latinas. Creado con HTML, CSS Y JS Vainilla. Manejo de data a través de Firebase. ",
+                "time": "Septiembre 2022",
+                "image": "https://user-images.githubusercontent.com/106553998/192071278-9bae4b81-0c02-4125-bd73-401b0d5c6d96.png",
+                "demo": "https://carolinafuenzalida.github.io/SCL021-social-network/src/index.html#/login",
+                "repo": "https://github.com/CarolinaFuenzalida/SCL021-social-network",
+            },
+        {
                 "name": "Ghiblipedia",
                 "about": "Aplicación web que permite al usuario visualizar, filtrar, y ordenar data con temática de las películas de Estudio Ghibli. Utilizando HTML, CSS Y JS Vainilla.",
                 "time": "Julio 2022",
@@ -17,21 +24,13 @@ export default {
                 "demo": "https://carolinafuenzalida.github.io/SCL021-data-lovers/src/index.html",
                 "repo": "https://github.com/CarolinaFuenzalida/SCL021-data-lovers",
             },
-            {
-                "name": "GG-Girls Social Network",
-                "about": "Red social GG-Girls enfocada en comunidad Gamer de Mujeres latinas. Incluye creación de Usuario, Ingreso con Google y Posts, manejo de data a través de Firebase. Utilizando HTML, CSS Y JS Vainilla.",
-                "time": "Septiembre 2022",
-                "image": "https://user-images.githubusercontent.com/106553998/192071278-9bae4b81-0c02-4125-bd73-401b0d5c6d96.png",
-                "demo": "https://carolinafuenzalida.github.io/SCL021-social-network/src/index.html#/login",
-                "repo": "https://github.com/CarolinaFuenzalida/SCL021-social-network",
-            },
-            {
-                "name": "Sanrio Coffee",
-                "about": "Web app con vistas de mesero para tomar pedidos y vista de cocina para visualizar los pedidos a preparar, manejo de data a través de Firebase. Utilizando React.",
-                "time": "Noviembre 2022",
-                "image": "https://user-images.githubusercontent.com/108352521/208190958-940046b3-fc7e-4736-b872-9eaa2076d864.png",
-                "demo": "https://sanriocoffee.netlify.app/",
-                "repo": "https://github.com/CarolinaFuenzalida/SCL021-burger-queen",
+        { 
+            "name": "Cipher - Spy X Family",
+            "about": "Aplicación web que permite al usuario cifrar y descifrar mensajes aplicando la técnica de Cifrado Cesar. Utilizando HTML, CSS Y JS Vainilla.",
+            "time": "Junio 2022",
+            "image": "https://user-images.githubusercontent.com/108352521/212223642-8b49ca18-71e2-4601-92f7-2bc421ac4904.png",
+            "demo": "https://carolinafuenzalida.github.io/SCL021-cipher/src/index.html",
+            "repo": "https://github.com/CarolinaFuenzalida/SCL021-cipher"
             },
         ]
     }
